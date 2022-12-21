9d05666135f9e78419d9fa365fd4c08f9fbe2cbc
alsqja <alsqja2626@uno-solution.com>
2022-12-21 16:59:22 +0900

    add put myinfo

--- routes/user.js
+++ routes/user.js
@@ -5,5 +5,6 @@ const { verifyToken } = require("./middlewares");
 const router = express.Router();
 
 router.get("/me", verifyToken, controller.user.getMe);
+router.put("/me", verifyToken, controller.user.put);
 
 module.exports = router;
