c1d4ba2acb7029e3985c48131597122d04af6452
alsqja <alsqja2626@uno-solution.com>
2022-12-27 09:06:44 +0900

    add update posts api

--- controller/post.js
+++ controller/post.js
@@ -31,6 +31,42 @@ module.exports = {
       return next(err);
     }
   },
+  put: async (req, res, next) => {
+    const { contents, urls } = req.body;
+    const accessTokenData = isAuthorized(req);
+    const { id } = accessTokenData;
+    const post_id = Number(req.params.id);
+
+    try {
+      const updated = await posts.update(
+        {
+          contents,
+          user_id: id,
+        },
+        {
+          where: { id: post_id },
+        }
+      );
+      await images.destroy({ where: { post_id } });
+      const imgUrls = [];
+      for (let i = 0; i < urls.length; i++) {
+        const newImg = await images.create({
+          user_id: id,
+          post_id,
+          url: urls[i],
+        });
+        imgUrls.push(newImg.url);
+      }
+      console.log(updated);
+      return res.status(201).send({
+        data: { id: post_id },
+        message: "게시글이 수정되었습니다.",
+      });
+    } catch (err) {
+      console.error(`new post error : ${err}`);
+      return next(err);
+    }
+  },
   delete: async (req, res, next) => {
     const { id } = req.params;
     const accessTokenData = isAuthorized(req);
